fac73cd503ea79188aad358db0ca1f5626aa9f1d
Dakota Rennemann <rennemannd@gmail.com>
2020-07-21 11:26:06 -0400

    Added logic for pr's not meant to be release

--- .github/actions/action-1/index.js
+++ .github/actions/action-1/index.js
@@ -7,6 +7,8 @@ const pr = context.payload.pull_request;
 const client = new GitHub(core.getInput("repo_token"));
 //const slackToken = core.getInput("slack_token");
 
+run();
+
 async function run() {
     // verify the pull request was merged
     if(!pr.merged) {
@@ -18,32 +20,52 @@ async function run() {
         let isRelease = pr.title.match(/release/gi);
         if (isRelease) {
             let version = pr.title.match(/(?:v\s?)(\d+.?)+/gi)[0];
-            let amendedVersion = "v" + version;
-            console.log(`Creating annotated tag`);
 
-            const tagCreateResponse = await client.git.createTag({
-                ...context.repo,
-                tag: amendedVersion,
-                message: pr.body,
-                object: context.sha,
-                type: "commit",
-            });
+            await postTag(version);
 
-            console.log(`Pushing annotated tag to the repo`);
-
-            let response = await client.git.createRef({
-                ...context.repo,
-                ref: `refs/tags/${amendedVersion}`,
-                sha: tagCreateResponse.data.sha,
-            });
-
-            console.log("Tag should be created, response was: \n\n", response);
         }  else {
-            console.log("This was a push to master which was not a release, attempting to increment patch version.");
+            console.log("This was a merge to master which was not a release, attempting to increment patch version.");
+
+            const previousTagSha = (
+                await exec("git rev-list --tags --topo-order --max-count=1")
+            ).trim();
+
+            let tag = await exec(`git describe --tags ${previousTagSha}`).trim();
+            let regTag = tag.match(/(?:v)(\d+.\d+).?(\d+)?/i);
+            let newVersion;
+
+            if(regTag[2]) {
+                let newPatch = parseInt(regTag[2], 10) + 1;
+                newVersion = regTag[1] + "." + newPatch.toString()
+            } else {
+                newVersion = regTag[1] + ".1";
+            }
+
+            await postTag(newVersion);
         }
     } catch (error) {
         core.warning(error.message);
     }
 }
 
-run();
+async function postTag(ver) {
+    console.log(`Creating annotated tag`);
+
+    const tagCreateResponse = await client.git.createTag({
+        ...context.repo,
+        tag: ver,
+        message: pr.body,
+        object: context.sha,
+        type: "commit",
+    });
+
+    console.log(`Pushing annotated tag to the repo`);
+
+    let response = await client.git.createRef({
+        ...context.repo,
+        ref: `refs/tags/${ver}`,
+        sha: tagCreateResponse.data.sha,
+    });
+
+    console.log("Tag should be created, response was: \n\n", response);
+}
