be85f2200b5dc0febc665cb61ae9f92f80306a29
Dakota Rennemann <rennemannd@gmail.com>
2020-07-21 08:27:02 -0400

    Creating and pushing tag to github ref

--- .github/actions/action-1/index.js
+++ .github/actions/action-1/index.js
@@ -12,39 +12,37 @@ async function run() {
         core.debug("Closed pr onto master was not merged, aborting action");
     }
 
-    core.debug("Pushing pull request to test sha values: \n\n");
-    console.log(pr);
 
-    // try {
-    //
-    //     let isRelease = pr.title.match(/release/gi);
-    //     if (isRelease) {
-    //         let version = pr.match(/(?:v\s?)(\d+.?)+/gi)[0];
-    //         let amendedVersion = "v" + version;
-    //         core.debug(`Creating annotated tag`);
-    //
-    //         const tagCreateResponse = await client.git.createTag({
-    //             ...context.repo,
-    //             tag: amendedVersion,
-    //             message: pr.body,
-    //             object: GITHUB_SHA,
-    //             type: "commit",
-    //         });
-    //
-    //         core.debug(`Pushing annotated tag to the repo`);
-    //
-    //         await octokit.git.createRef({
-    //             ...context.repo,
-    //             ref: `refs/tags/${newTag}`,
-    //             sha: tagCreateResponse.data.sha,
-    //         });
-    //
-    //     }  else {
-    //         console.log("This was a push to master which was not a release, attempting to increment patch version.");
-    //     }
-    // } catch (error) {
-    //     core.warning(error.message);
-    // }
+    try {
+
+        let isRelease = pr.title.match(/release/gi);
+        if (isRelease) {
+            let version = pr.match(/(?:v\s?)(\d+.?)+/gi)[0];
+            let amendedVersion = "v" + version;
+            core.debug(`Creating annotated tag`);
+
+            const tagCreateResponse = await client.git.createTag({
+                ...context.repo,
+                tag: amendedVersion,
+                message: pr.body,
+                object: context.sha,
+                type: "commit",
+            });
+
+            core.debug(`Pushing annotated tag to the repo`);
+
+            await client.git.createRef({
+                ...context.repo,
+                ref: `refs/tags/${amendedVersion}`,
+                sha: tagCreateResponse.data.sha,
+            });
+
+        }  else {
+            console.log("This was a push to master which was not a release, attempting to increment patch version.");
+        }
+    } catch (error) {
+        core.warning(error.message);
+    }
 }
 
 run();
